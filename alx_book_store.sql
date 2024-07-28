addfbab03de0bb9858555489e7782c105729f564
osasenaga <edionwe.osasenaga5@gmail.com>
2024-07-28 13:50:53 +0100

    DATABASE

---
+++ alx_book_store.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE alx_book_store;
+
+USE alx_book_store;
+
+CREATE TABLE AUTHORS (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
+);
+
+CREATE TABLE BOOKS (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREAT TABLE CUSTOMERS (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT
+);
+
+CREATE TABLE ORDERS (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+);
+
+CREATE TABLE ORDER_DETAILS (
+    order_detail_id INT PRIMARY KEY AUTO INCREMENT,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE
+    FOREIGN KEY (Order_id) REFERENCES ORDERS(Order_id),
+    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+);
