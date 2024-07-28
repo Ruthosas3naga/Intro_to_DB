5105dc49f8e61a2553d555362ee181ed02015df9
osasenaga <edionwe.osasenaga5@gmail.com>
2024-07-28 21:20:26 +0100

    task 4

---
+++ task_4.sql
@@ -0,0 +1,9 @@
+-- File name: task_4.sql
+
+-- Use the specified database
+USE alx_book_store;
+
+-- Query to get the full description of the table 'books'
+SELECT book_id, title, author_id, price, publication_date 
+FROM BOOKS;
+
