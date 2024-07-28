15dd8c57200ffe86a62a795761aabefbaa6d29a2
osasenaga <edionwe.osasenaga5@gmail.com>
2024-07-28 21:33:28 +0100

    task 5

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 USE alx_book_store;
 
-INSER INTO customers (customer_id, customer_name, email, address),
+INSER INTO customer (customer_id, customer_name, email, address),
 VALUES ("1", "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave");
