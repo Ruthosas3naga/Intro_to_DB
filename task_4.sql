c67d7c445b262e7e96d6221fd24251fb97ef2e43
osasenaga <edionwe.osasenaga5@gmail.com>
2024-07-28 21:23:01 +0100

    task 4

--- task_4.sql
+++ task_4.sql
@@ -5,7 +5,7 @@ USE alx_book_store;
 
 -- Query to get the full description of the table 'books'
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
 
 
